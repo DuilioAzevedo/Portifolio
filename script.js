07182683d61fb3fcb218ba80e6d42449c82a68b5
duilio_uei@hotmail.com <duilio_uei@hotmail.com>
2023-08-03 02:08:50 -0300

    Back to top button

--- script.js
+++ script.js
@@ -82,3 +82,16 @@
 
         portfolioIsotope.isotope({filter: $(this).data('filter')});
     });
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
